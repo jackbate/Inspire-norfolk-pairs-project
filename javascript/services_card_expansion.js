0142da2f121bf205883c44d6c2dd2703735fad92
Zach Gilbert <gilberza@gmail.com>
2021-02-08 09:44:16 +0000

    Made JS unobtrusive: cards default expanded using CSS, and toggles to collapsed with JS

--- javascript/services_card_expansion.js
+++ javascript/services_card_expansion.js
@@ -1,3 +1,74 @@
+
+// COLLAPSED:
+
+function collapseCard (container, description, fade, button) {
+  container.style.margin = '35px auto';
+  container.style.padding = '0 15px';
+  container.style.minWidth = '0';
+
+  description.style.maxHeight = '120px';  // 5 * line height
+  fade.style.visibility = 'visible';
+
+  button.textContent = 'Read More';
+
+  button.classList.remove('expanded');
+  button.classList.add('collapsed');
+
+}
+
+// EXPANDED:
+
+function expandCard (container, description, fade, button) {
+  container.style.margin = '35px 0';
+  container.style.padding = '0';
+  container.style.minWidth = '100%';
+
+  description.style.maxHeight = 'max-content';
+  fade.style.visibility = 'hidden';
+
+  button.textContent = 'Read Less';
+
+  button.classList.remove('collapsed');
+  button.classList.add('expanded');
+}
+
+
+const buttons = document.getElementsByClassName('service-card-button');
+const fades = document.getElementsByClassName('fade');
+
+for (let i = 0; i <= buttons.length - 1; i++) {
+  let cardButton = buttons[i];
+  let cardTextFade = fades[i];
+  cardButton.addEventListener('click', () => {
+
+    let cardBody = cardButton.parentNode;
+    let cardContainer = cardBody.parentNode;
+    let cardDescription = cardButton.previousElementSibling;
+
+    if (cardButton.classList.contains('collapsed')) {
+      expandCard(
+        cardContainer,
+        cardDescription,
+        cardTextFade,
+        cardButton);
+    } else {
+      collapseCard(
+        cardContainer,
+        cardDescription,
+        cardTextFade,
+        cardButton);
+    }
+  });
+}
+
+
+
+
+
+
+
+
+/*
 const buttons = document.getElementsByClassName('service-card-button');
 const fades = document.getElementsByClassName('fade');
 
@@ -38,3 +109,4 @@ for (let i = 0; i <= buttons.length - 1; i++) {
     }
   });
 }
+*/
